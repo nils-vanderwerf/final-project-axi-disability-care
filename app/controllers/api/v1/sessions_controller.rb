467e3e3d0daf3cd0fa4d89a9710f07b92560d73d
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-07-28 16:01:49 +1000

    Refresh page not working

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,38 +1,37 @@
 class Api::V1::SessionsController < ApplicationController
-	include CurrentUserConcern
-		def create
-			user = User
-					.find_by(email: params["user"]["email"])
-					.try(:authenticate, params["user"]["password"])
-			if user
-				session[:user_id] = user.id
-				render json: {
-					status: :created,
-					logged_in: true,
-					user: user
-				}
-			else
-				render json: { status: 401 }
-				flash.now[:danger]="Email/Password is not matching"
-			end
-		end
-
-		def get_current_user
-            if @current_user
-                render json: {
-                    logged_in: true,
-                    user: @current_user
-                }
-            else
-                render json: {
-                    logged_in: false
-                }
-            end
+    include CurrentUserConcern
+	skip_before_action :set_current_user, only: :create
+    def create
+        user = User
+                .find_by(email: params["user"]["email"])
+                .try(:authenticate, params["user"]["password"])
+        if user
+            session[:user_id] = user.id
+			
+            render json: {
+                status: :created,
+                logged_in: true,
+                user: user
+            }
+        else
+            render json: { status: 401 }
         end
-#
-		def logout
-			p 'Logout function reached'
-			render json: { status: 200, logged_out: true}
-		end
-	
 	end
+	def get_current_user
+		if @current_user
+			render json: {
+				logged_in: true,
+				user: @current_user
+			}
+		else
+			
+			render json: {
+				logged_in: false
+			}
+		end
+	end
+	def logout
+		reset_session
+		render json: { status: 200, logged_out: true}
+	end
+end
--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,10 +6,19 @@ module CurrentUserConcern
     end
   
     def set_current_user
-      
       if session[:user_id]
+        puts "Session id: #{session[:user_id]}"
         @current_user = User.find(session[:user_id])
-      end
+      else
+         p "cookies: #{cookies[:name]}"
+          # user = User
+          # .find_by(email: params["user"]["email"])
+          # .try(:authenticate, params["user"]["password"])
+          p "Found the user #{user}"
+          if user
+            @current_user = user
+            session[:user_id] = user.id
+        end
     end
 end
-  
+end
